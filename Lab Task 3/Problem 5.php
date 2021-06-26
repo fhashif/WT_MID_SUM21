e58abda66b4e6e4d47c0f2b0958314225d5d544c
Your Name <you@example.com>
2021-06-27 02:25:55 +0600

    lab task completed

--- Lab Task 3/Problem 5.php
+++ Lab Task 3/Problem 5.php
@@ -1,7 +1,15 @@
 <?php
  $backColor="green";$fontColor="red";$imageWidth="80px";$imageHeight="80px";
 ?>
-<img src="aiub_logo.jpg" width="<?php echo "$imageWidth"?>" height="<?php echo "$imageHeight"?>"/>
-<h2>American International University-<span style="background-color:<?php echo "$backColor"?>;color:<?php echo "$fontColor"?>">
-Bangladesh</h2>
-</span>
+<table>
+ <tr>
+  <td>
+	<img src="aiub_logo.jpg" width="<?php echo "$imageWidth"?>" height="<?php echo "$imageHeight"?>"/>
+  </td>
+  <td>
+	<h2>American International<br>University-<span style="background-color:<?php echo "$backColor"?>;color:<?php echo "$fontColor"?>">
+	Bangladesh
+	</span></h2>
+  </td>
+ </tr>
+</table>
